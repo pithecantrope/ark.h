a7ec7ad45cd5c00cbe7e4a84c64a85e84d17994a
pithecantrope <fartdraft@gmail.com>
2026-05-01 18:52:46 +0300

    v0.0

---
+++ src/ark.h
@@ -0,0 +1,8 @@
+#ifndef ARK_HEADER
+#define ARK_HEADER
+
+#define ARK_VERSION_MAJOR 0
+#define ARK_VERSION_MINOR 0
+#define ARK_VERSION       "0.0"
+
+#endif // ARK_HEADER
---
+++ src/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "ark.h"
+
+int
+main(void) {
+        printf("Ark is version %s\n", ARK_VERSION);
+        return 0;
+}
