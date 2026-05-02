9a46ccc1958d56e6592de3c2910c55dbe5a23c64
pithecantrope <fartdraft@gmail.com>
2026-05-02 20:37:56 +0300

    Skeleton

--- src/ark.h
+++ src/ark.h
@@ -1,8 +1,22 @@
-#ifndef ARK_HEADER
-#define ARK_HEADER
+#ifndef ARK_H
+#define ARK_H
 
 #define ARK_VERSION_MAJOR 0
 #define ARK_VERSION_MINOR 0
 #define ARK_VERSION       "0.0"
 
-#endif // ARK_HEADER
+#if !(defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202000L))
+#error "Ark requires C23 or later"
+#endif
+
+#ifndef ARK_API
+#define ARK_API extern
+#endif // ARK_API
+
+#ifdef ARK_IMPLEMENTATION
+#endif // ARK_IMPLEMENTATION
+
+#ifndef ARK_PREFIXED
+#endif // ARK_PREFIXED
+
+#endif // ARK_H
